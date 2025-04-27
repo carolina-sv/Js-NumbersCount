7c48999251c779640bcec606c080daf4c32f4b61
Carolina Viana <carollinasilva057@gmail.com>
2025-04-27 11:03:06 -0300

    feat(all): finish the main part

---
+++ script.js
@@ -0,0 +1,12 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+// console.log(valueDisplays);
+
+valueDisplays.forEach((valueDisplay) => {
+    let starValue = 0;
+    let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+
+
+
+});
